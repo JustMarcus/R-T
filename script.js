2ee4e0bac9b306962fb3bc19b22fb038316e0cba
JustMarcus <87469373+JustMarcus@users.noreply.github.com>
2021-07-15 12:46:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function getInputValue() {
   
         let inputVal = document.getElementById("inputId").value;
   
-  secrets.SECRET_PASSWORD
+ PASSWORD: ${{ secrets.SECRET_PASSWORD }}
   
 "document.write(5 + 6)"
   

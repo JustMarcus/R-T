1f88c96ece5fe4a1211d414cc6c43c3b60455e25
JustMarcus <87469373+JustMarcus@users.noreply.github.com>
2021-07-15 02:49:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function getInputValue() {
   
   secrets.SECRET_PASSWORD
   
-  if (window.location.href==="https://xyz.github.io") {
-    window.location.href = "https://www.youtube.com/"; 
-}
+
   
         console.log(inputVal);
       }

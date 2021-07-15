4d980950ef13f071a6b79f7c23d61bfb01722ad6
JustMarcus <87469373+JustMarcus@users.noreply.github.com>
2021-07-15 01:45:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function getInputValue() {
 
   
+  const audio = new Audio("bellaciao.mp3");
+  
+  audio.play();
+  
+  
+  
         let inputVal = document.getElementById("inputId").value;
   
   secrets.SECRET_PASSWORD

92ee4b432489624dd557329d0790dfbec71380c7
JustMarcus <87469373+JustMarcus@users.noreply.github.com>
2021-07-15 13:24:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-    function getInputValue() {
 
-
-let inputVal = document.getElementById("inputId").value;
-
-        if (inputVal === '27') {
-        alert("Correct!");
-        } else {
-    alert(inputVal + "<-- Value";
-    }
-}

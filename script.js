fd14b77573d49ce4e05e2cca45a1cbd52909f98c
JustMarcus <87469373+JustMarcus@users.noreply.github.com>
2021-07-15 01:35:43 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,10 @@ function getInputValue() {
 
   
         let inputVal = document.getElementById("inputId").value;
-
+  
+  secrets.SECRET_PASSWORD
+  
+  alert(secrets.SECRET_PASSWORD)
 
         console.log(inputVal);
       }

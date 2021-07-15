f5afc502d2e1c0e8c4e92a13dcc17dc23b4542c6
JustMarcus <87469373+JustMarcus@users.noreply.github.com>
2021-07-15 12:57:54 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function getInputValue() {
 
-
-demoA()
-  
-        let inputVal = document.getElementById("inputId").value;
-  
- PASSWORD: ${{ secrets.SECRET_PASSWORD }}
-  
-"document.write(5 + 6)"
-  
-        console.log(inputVal);
-      }
